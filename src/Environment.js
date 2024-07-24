3e5e7aee0b31e702e00302bf6f7e6482b6d41d3f
Lucas Emanoel <lucas2014.barros@gmail.com>
2024-07-24 16:54:11 -0300

    comment: positions for tests

--- src/Environment.js
+++ src/Environment.js
@@ -23,6 +23,7 @@ export class Environment {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
     this.startPos = { x: 1, y: 1 };
-    this.endPos = { x: 12, y: 16 };
+    //variar o 15 e 16 no y
+    this.endPos = { x: 6, y: 16 };
   }
 }
